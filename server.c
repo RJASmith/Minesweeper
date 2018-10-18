5babb6237becb671e01c9a260fea240072e68cc5
RJASmith <reillysmith4444@gmail.com>
2018-10-18 11:13:03 +1000

    Mine Placing Changed

--- server.c
+++ server.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
 #include <string.h>
 #define RANDOM_NUMBER_SEED 42
 #define NUM_MINES 10
 #define NUM_TILES_X 9
 #define NUM_TILES_Y 9
+#define TRUE 1
+#define FALSE 0
 
 typedef struct {
   int adjacent_mines;
@@ -17,88 +20,35 @@ typedef struct {
   Tile tiles[NUM_TILES_X][NUM_TILES_Y];
 } GameState;
 
-void place_mines(GameState tiles){
+GameState initialiseGamestate() {
+  GameState gameState;
+  for(int a = 0; a < NUM_TILES_X; a++) {
+    for(int b = 0; b < NUM_TILES_Y; b++) {
+        gameState.tiles[a][b].adjacent_mines = 0;
+        gameState.tiles[a][b].revealed = FALSE;
+        gameState.tiles[a][b].is_mine = FALSE;
+    }
+  }
+  return gameState;
+}
+
+bool tile_contains_mine(GameState gameState, int x, int y){
+      return gameState.tiles[x][y].is_mine;
+}
+
+void place_mines(GameState gameState){
   for(int i = 0; i < NUM_MINES; i++) {
     int x, y;
     do{
       x = rand() % NUM_TILES_X;
       y = rand() % NUM_TILES_Y;
-    } while (tile_contains_mine(x,y));
+  } while (tile_contains_mine(gameState,x,y));
     //place mine at (x,y)
-    tiles[x][y]->is_mine = TRUE;
+    gameState.tiles[x][y].is_mine = TRUE;
   }
 }
 
-bool tile_contains_mine(int x, int y){
 
-
-}
-//Function to generate the x and y positions for the mines
-int generateMineLocations(){
-  //defining the data for the nodes in our linked list which is storing the mine locations
-  typedef struct node{
-    int x;
-    int y;
-    struct node * next;
-  } node_t;
-
-  //Defining the head pointer for the linked list
-  node_t * head = NULL;
-  head = malloc(sizeof(node_t));
-  if(head == NULL) {
-    return 1;
-  }
-
-  //looping 10 times to create the mine locations
-  for(int i = 0; i < MAX_MINES; i++){
-    //set the current node in the list to the head
-    node_t * current = head;
-    //iterates until it finds an empty node
-    while(current->next != NULL) {
-      //If the next node isn't empty, set the current node to be the next one
-      current = current->next;
-    }
-    int cont = 0;
-    int x = 0;
-    int y = 0;
-    while(cont == 0){
-      //Generate two random integers
-      x = rand() % GRID_SIZE;
-      y = rand() % GRID_SIZE;
-
-      //ensure this variable is set to 0 each loop
-      int unique = 0;
-      //loop through and see if the generated numbers match others in the list
-      node_t * currentTwo = head;
-      while(currentTwo->next != NULL) {
-        if(currentTwo->x == x && currentTwo->y == y){
-          //if they match, increment integer, which means this will have to loop again.
-          unique++;
-        }
-        //move onto the next node
-        currentTwo = currentTwo->next;
-      }
-      //if no matches were found, end the while loop
-      if(unique == 0){
-        cont = 1;
-      }
-    }
-    //create the next node and allocate it the two random digits created before
-    current->next = malloc(sizeof(node_t));
-    current->next->x = x;
-    current->next->y = y;
-
-  }
-  //For testing. Loop through and print the integers stored
-  node_t * current = head;
-  while(current->next != NULL) {
-    printf("x = %d; y = %d\n", current->next->x, current->next->y);
-
-    current = current->next;
-  }
-
-  return 0;
-}
 
 int checkUsernamePassword(char username[20], char password[20]) {
   char storedPassword[20] = "Password";
@@ -112,7 +62,6 @@ int checkUsernamePassword(char username[20], char password[20]) {
   else{
     printf("Login Unsuccessful");
   }
-
   return 1;
 }
 
