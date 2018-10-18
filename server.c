4c5ce40df3b558dcb398c2d5711bd3d1e1ab32d5
RJASmith <reillysmith4444@gmail.com>
2018-10-18 10:33:17 +1000

    Seg Fault Fixed, changed tile storage

--- server.c
+++ server.c
@@ -2,6 +2,37 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#define RANDOM_NUMBER_SEED 42
+#define NUM_MINES 10
+#define NUM_TILES_X 9
+#define NUM_TILES_Y 9
+
+typedef struct {
+  int adjacent_mines;
+  bool revealed;
+  bool is_mine;
+} Tile;
+
+typedef struct {
+  Tile tiles[NUM_TILES_X][NUM_TILES_Y];
+} GameState;
+
+void place_mines(GameState tiles){
+  for(int i = 0; i < NUM_MINES; i++) {
+    int x, y;
+    do{
+      x = rand() % NUM_TILES_X;
+      y = rand() % NUM_TILES_Y;
+    } while (tile_contains_mine(x,y));
+    //place mine at (x,y)
+    tiles[x][y]->is_mine = TRUE;
+  }
+}
+
+bool tile_contains_mine(int x, int y){
+
+
+}
 //Function to generate the x and y positions for the mines
 int generateMineLocations(){
   //defining the data for the nodes in our linked list which is storing the mine locations
@@ -17,11 +48,9 @@ int generateMineLocations(){
   if(head == NULL) {
     return 1;
   }
-  //generating a random seed for the mine location generation
-  srand(time(NULL));
 
   //looping 10 times to create the mine locations
-  for(int i = 0; i < 10; i++){
+  for(int i = 0; i < MAX_MINES; i++){
     //set the current node in the list to the head
     node_t * current = head;
     //iterates until it finds an empty node
@@ -34,8 +63,8 @@ int generateMineLocations(){
     int y = 0;
     while(cont == 0){
       //Generate two random integers
-      x = rand() % 10;
-      y = rand() % 10;
+      x = rand() % GRID_SIZE;
+      y = rand() % GRID_SIZE;
 
       //ensure this variable is set to 0 each loop
       int unique = 0;
@@ -71,30 +100,31 @@ int generateMineLocations(){
   return 0;
 }
 
-int checkPassword(char *username, char *password) {
-  printf("Inside Func");
-  char *storedPassword = "Password";
-  char *storedUsername = "Reilly";
-  int uRet;
-  uRet = strcmp(storedPassword, password);
-  /*int pRet = strcmp(storedUsername, username);
+int checkUsernamePassword(char username[20], char password[20]) {
+  char storedPassword[20] = "Password";
+  char storedUsername[20] = "Reilly";
+  //int uRet;
+  int uRet = strcmp(storedPassword, password);
+  int pRet = strcmp(storedUsername, username);
   if(uRet == 0 && pRet == 0) {
     printf("Login Successful");
   }
   else{
     printf("Login Unsuccessful");
   }
-  */
+
   return 1;
 }
 
 void main(int argc, char argv) {
+  //generating a random seed for the mine location generation
+  srand(RANDOM_NUMBER_SEED);
   //generateMineLocations();
   printf("\nEnter Username: \n");
-  char *username;
+  char username[20];
   scanf("%s", username);
   printf("\nEnter Password: \n");
-  char *password;
+  char password[20];
   scanf("%s", password);
-  checkPassword(username, password);
+  checkUsernamePassword(username, password);
 }
